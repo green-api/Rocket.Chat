7a7db8cd6c757d6c1dccd1f3072471df3e88a6ec
ripreal <ripreal1@yandex.ru>
2021-04-07 10:07:02 +0300

    SF-38 Убрать подвал в шаблонах писем

--- app/lib/server/startup/email.js
+++ app/lib/server/startup/email.js
@@ -144,13 +144,15 @@ settings.addGroup('Email', function() {
 			multiline: true,
 			i18nLabel: 'Header',
 		});
+		// START GREEN API
 		// Убрать копирайт в письмах
-		// this.add('Email_Footer', '</td></tr></table></div></td></tr></table><!-- /BODY --></td></tr><tr style="margin: 0; padding: 0;"><td style="margin: 0; padding: 0;"><!-- FOOTER --><table class="wrap"><tr><td class="container"><!-- content --><div class="content"><table width="100%"><tr><td align="center" class="social"><a href="https://rocket.chat/blog">Blog</a> | <a href="https://github.com/RocketChat">Github</a> | <a href="https://www.facebook.com/RocketChatApp">Facebook</a> | <a href="https://www.instagram.com/rocket.chat">Instagram</a></td></tr><tr><td align="center"><h6>© Rocket.Chat Technologies Corp.</h6><h6>Made with ❤️ in 🇧🇷 🇨🇦 🇩🇪 🇮🇳 🇬🇧 🇺🇸 </h6></td></tr></table></div><!-- /content --></td></tr></table><!-- /FOOTER --></td></tr></table></body></html>', {
-		//	type: 'code',
-		//	code: 'text/html',
-		//	multiline: true,
-		//	i18nLabel: 'Footer',
-		// });
+		 this.add('Email_Footer', '</td> </tr> </table> </div> </td> </tr> </table><!-- /BODY --> </td> </tr> <tr style="margin: 0; padding: 0;"> <td style="margin: 0; padding: 0;"> <!-- FOOTER --> <table class="wrap"> <tr> <td class="container"> <!-- content --> <div class="content"> <table width="100%"> <tr> <td align="center" class="social"></td> </tr> <tr> <td align="center"> <h6>Slonbook</h6> </td> </tr> </table> </div><!-- /content --> </td> </tr> </table><!-- /FOOTER --> </td> </tr> </table> </body> </html>', {
+			type: 'code',
+			code: 'text/html',
+			multiline: true,
+			i18nLabel: 'Footer',
+		 });
+		// END GREEN API
 		this.add('Email_Footer_Direct_Reply', '<p class="advice">{Direct_Reply_Advice}</p>', {
 			type: 'code',
 			code: 'text/html',
