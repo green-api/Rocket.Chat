4d41c8619cabdef9ff782dbe4df3dbb9e5dbe7e4
ripreal <ripreal1@yandex.ru>
2021-04-07 22:03:14 +0300

    fix pull-request CI

--- builddocker.sh
+++
@@ -1,3 +0,0 @@
-meteor build build
-tar -xvzf build/Rocket.Chat.tar.gz -C .docker
-docker build . -t registry.gitlab.com/green-api/docker/rocketchat --no-cache
--- publishdocker.sh
+++
@@ -1,4 +0,0 @@
-docker image tag registry.gitlab.com/green-api/docker/rocketchat  registry.gitlab.com/green-api/docker/rocketchat:1.0.0
-docker image tag registry.gitlab.com/green-api/docker/rocketchat:1.0.0 registry.gitlab.com/green-api/docker/rocketchat:latest
-docker push registry.gitlab.com/green-api/docker/rocketchat:1.0.0
-docker push registry.gitlab.com/green-api/docker/rocketchat:latest
